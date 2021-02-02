f7ca316499caa12b5230d23afb92b465831fae28
Nick Dumas <drakonik@gmail.com>
2021-02-02 11:47:33 -0500

    Big rewrite of Normalize() and main()
    Decided to actually use my walkfunc, and i clearly need to rethink how
    i'm adding entries to the chain cause i'm getting "word.Word" and other
    junk.
    
    But first, I need to be able to dump this to JSON so I can inspect it
    more easily.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -15,11 +16,19 @@ import (
 )
 
 // Normalize takes an input string and returns a slice of whitespace spearated words in all lowercase with all punctuation removed.
-func Normalize(s string) []string {
+func Normalize(entries []string) []string {
+	var res []string
 	// This regex is neat. \p{L} means "any letter in any language". \p{Z} means "any whitespace character in any unicode language". I'm using these so the markov engine can be 100% unicode friendly and language agnostic.
-	reg, _ := regexp.Compile(`[^\p{L}\p{Z}]+`)
-	words := strings.Split(strings.ToLower(reg.ReplaceAllString(s, "")), " ")
-	return words
+	reg := regexp.MustCompile(`[^\p{L}\p{Z}]+`)
+	for _, e := range entries {
+		split := reg.Split(e, -1)
+		for _, w := range split {
+			res = append(res, w)
+		}
+		// res[0] = reg.ReplaceAllString(
+		//words := strings.Split(strings.ToLower(reg.ReplaceAllString(s, "")), " ")
+	}
+	return res
 
 }
 
@@ -44,12 +53,26 @@ func W(c *m.Chain) filepath.WalkFunc {
 			return err
 		}
 
-		raw, err := LoadFile(path)
+		// raw, err := LoadFile(path)
+		f, err := os.Open(path)
+		defer f.Close()
 		if err != nil {
-			log.Printf("Unable to load %s.\n", path)
+			log.Printf("Unable to open file %s: %s\n", path, err)
 			return err
 		}
-		c.Add(Normalize(raw))
+
+		var (
+			s       string
+			entries []string
+		)
+
+		for {
+			if _, err := fmt.Fscan(f, &s); err != nil {
+				break
+			}
+			entries = append(entries, s)
+		}
+		c.Add(Normalize(entries))
 		return nil
 	}
 	return wf
@@ -85,23 +108,53 @@ func Text(c *m.Chain, x, y int) string {
 }
 
 func main() {
-	var fname string
+	var walkdir, brainpath string
 	var order, min, max int
 
 	flag.IntVar(&order, "order", 1, "Ordinality of Markov chains.")
-	flag.IntVar(&min, "min", 10, "Minimum output word count.")
-	flag.IntVar(&max, "max", 30, "Maximum output word count.")
-	flag.StringVar(&fname, "filename", "corpus.txt", "path to file containing corpus data")
+	flag.IntVar(&min, "min", 30, "Minimum output word count.")
+	flag.IntVar(&max, "max", 90, "Maximum output word count.")
+	flag.StringVar(&walkdir, "walkdir", ".", "path to directory containing corpus data")
+	flag.StringVar(&brainpath, "brainpath", "", "path to file containing a brain")
 
 	flag.Parse()
 
-	raw, err := LoadFile(fname)
-	if err != nil {
-		log.Fatalf("%s", err)
+	var c *m.Chain
+
+	if brainpath != "" {
+		brain, err := os.Open(brainpath)
+		defer brain.Close()
+		if err == os.ErrNotExist {
+			log.Fatalf("brain doesn't exist for loading: %s", err)
+		}
+		if err != nil {
+			log.Fatalf("error loading brain file: %s", err)
+		}
+
+		dec := json.NewDecoder(brain)
+		err = dec.Decode(c)
+		if err != nil {
+			log.Fatalf("couldn't load brain into []byte: %s", err)
+		}
 	}
-	corpus := Normalize(raw)
-	c := m.NewChain(order)
-	c.Add(corpus)
+	c = m.NewChain(order)
+
+	wf := W(c)
+	filepath.Walk(walkdir, wf)
+	/*
+		raw, err := LoadFile(fname)
+		if err != nil {
+			log.Fatalf("%s", err)
+		}
+		corpus := Normalize(raw)
+		c.Add(corpus)
+	*/
 
 	fmt.Println(Text(c, min, max))
+	f, err := os.OpenFile(brainpath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatalf("couldn't open brain file for dumping: %s", err)
+	}
+	enc := json.NewEncoder(f)
+	enc.Encode(c)
 }
