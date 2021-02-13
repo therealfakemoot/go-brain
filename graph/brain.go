5ce4178aad79fa75cedcdec873cc37753e63156f
Nick Dumas <drakonik@gmail.com>
2021-02-13 15:36:25 -0500

    Partial graph brain stub

--- graph/brain.go
+++ graph/brain.go
@@ -1,1 +1,18 @@
 package graph
+
+import (
+	gonum "gonum.org/v1/gonum/graph/simple"
+)
+
+// NewBrain creates a gonum.DirectedWeightedGraph powered markov brain with a key order of n
+func NewBrain(n int) Brain {
+	var b Brain
+	b.g = gonum.NewWeightedDirectedGraph(0.0, 0.0)
+
+	return b
+}
+
+// Brain is a gonum.DirectedWeightedGraph powered markov brain
+type Brain struct {
+	g *gonum.WeightedDirectedGraph
+}
