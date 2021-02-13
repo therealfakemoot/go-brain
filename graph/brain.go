583bb146da4a56bcf7a2e39b2912f6880955c4d9
Nick Dumas <drakonik@gmail.com>
2021-02-13 18:26:40 -0500

    First draft of Learn() and Generate()
    Learn() seems to work fine. Serializing is broken.
    
    Generate() is currently goofy as heck.

--- graph/brain.go
+++ graph/brain.go
@@ -1,18 +1,95 @@
 package graph
 
 import (
-	gonum "gonum.org/v1/gonum/graph/simple"
+	"bufio"
+	"io"
+	"math/rand"
+	"strings"
+
+	"github.com/therealfakemoot/copy-bot"
+
+	ggraph "gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/simple"
 )
+
+// Brain is a gonum.DirectedWeightedGraph powered markov brain
+type Brain struct {
+	o int // how many consecutive words to use as each vertex
+	g *simple.WeightedDirectedGraph
+}
 
 // NewBrain creates a gonum.DirectedWeightedGraph powered markov brain with a key order of n
 func NewBrain(n int) Brain {
 	var b Brain
-	b.g = gonum.NewWeightedDirectedGraph(0.0, 0.0)
+	b.o = n
+	b.g = simple.NewWeightedDirectedGraph(0.0, 0.0)
 
 	return b
 }
 
-// Brain is a gonum.DirectedWeightedGraph powered markov brain
-type Brain struct {
-	g *gonum.WeightedDirectedGraph
+// Generate produces up to n strings from the stored brain juice
+func (b *Brain) Generate(n int) []string {
+
+	var s []string
+
+	weighted := make([]string, 1000)
+
+	// create the baseline start token, n empty strings
+	p := make(brain.Prefix, b.o)
+	n1 := Node{p.String()}
+
+	var matches []ggraph.WeightedEdge
+	var loopNode Node = n1
+	for len(s) < n {
+		matches = EdgesOf(b.g, loopNode)
+		for _, m := range matches {
+			w := m.Weight()
+			to := m.To().(Node)
+			for i := 0.0; i < w; i++ {
+				weighted = append(weighted, to.Key)
+			}
+		}
+		choice := weighted[rand.Intn(len(weighted))]
+		s = append(s, choice)
+		p.Shift(choice)
+		loopNode = Node{p.String()}
+		weighted = weighted[:]
+
+	}
+
+	return s
+}
+
+func (b *Brain) Learn(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	p := make(brain.Prefix, b.o)
+	for scanner.Scan() {
+		for _, s := range strings.Fields(scanner.Text()) {
+			n1 := Node{p.String()}
+			p.Shift(s)
+			n2 := Node{p.String()}
+			weight, ok := b.g.Weight(n1.ID(), n2.ID())
+			if !ok {
+				we := simple.WeightedEdge{
+					F: n1,
+					T: n2,
+					W: 1.0,
+				}
+				b.g.SetWeightedEdge(we)
+				continue
+			}
+
+			we := simple.WeightedEdge{
+				F: n1,
+				T: n2,
+				W: weight + 1.0,
+			}
+			b.g.SetWeightedEdge(we)
+		}
+	}
+}
+
+func (b *Brain) Save() error {
+
+	return nil
 }
